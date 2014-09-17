a0be8ebac4e8d6626d21b1c97f4a11fb1945f2be
coberlin <christina.oberlin@gmail.com>
2014-09-17 10:49:43 -0400

    moving config model

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.generators do |g|
-  g.orm :mongo_mapper
-end
+
 
 module MongoSample
   class Application < Rails::Application
@@ -23,6 +21,10 @@ module MongoSample
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.orm :mongo_mapper
+    end
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
